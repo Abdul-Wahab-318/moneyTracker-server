e983519ceabcd9dd7df94eb77f620bd39ffa7e76
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2022-08-02 07:47:33 +0500

    CORS changes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 // allows cross origin resource sharing
 app.use(cors({
-    origin : [ "http://localhost:3000" ] ,
+    origin : [ "http://localhost:3000" , "https://moneytracker-awm.netlify.app" ] ,
     credentials : true 
 }))
 
