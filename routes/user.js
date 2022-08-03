2ed70a4be128338c3ba7176d536e822d491207d8
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2022-08-03 23:36:11 +0500

    additional fixes to logout function

--- routes/user.js
+++ routes/user.js
@@ -84,8 +84,10 @@ router.post( "/logout" , auth , async ( req , res ) => {
 
     req.user = null 
     req.userID = null 
+    console.log("inside log out : " , req.cookies)
+    res.clearCookie("token" ,
+    { domain : "money-tracker-app-server.herokuapp.com", path : "/" , secure : true , sameSite : 'none'})
     console.log("logged out : " , req.cookies)
-    res.clearCookie("token" , { domain : "money-tracker-app-server.herokuapp.com" , path : "/"})
     res.status(200).json({ message : "logged out" , ok : true } )
 
 } )
